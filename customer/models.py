3748b93bf5450efa21c4599203420824070bda8b
David <david@rlabs.org>
2023-06-23 23:04:01 +0200

    abstract user

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+account_type = (
+    ('customer','customer'),
+    ('barista', 'barista'),
+)
+
+class profile(AbstractUser):
+    account_type =  models.CharField(max_length=12, choices=account_type, default='customer')
+    def __str__(self):
+        return self.email
